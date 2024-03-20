e94543662a5227b4358fc0e20d2dcb1f2768c858
Lior <liormdevops@gmail.com>
2024-03-20 10:13:57 +0200

    better system data commands for data-collector script

--- data-collector.sh
+++ data-collector.sh
@@ -2,8 +2,15 @@
 
 # Collect monitoring data
 mem_usage=$(free | awk "/Mem/{printf \$3/\$2*100}")
-cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk "{print 100 - \$1}")
-disk_status=$(df -h | grep '/$' | awk '{print $5}')
+
+cpu_usage=$(mpstat -P ALL | awk 'NR==1 {print $2, $3}; NR>2 {print $2, $3}')
+# mpstat -P ALL displays statistics about CPU usage for all CPUs individually
+# awk NR==1 {print $2, $3} prints the values of 2nd and 3rd filed in the first line of the output (Headers)
+# (awk) NR>2 {print $2, $3} conditional that prints 2nd and 3rd column of each record (line) from line 2 and on 
+
+disk_status=$(df | awk '$NF == "/" {print $(NF-1)}')
+# This command takes from df output the record where root volume is ($NF == "/")
+# and prints the % usage in one-before-last column {print $(NF-1)}
 
 # Write data to a file (you may modify this to your preferred format)
 echo "Memory Usage: ${mem_usage}%"
